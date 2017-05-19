e902f113995ae24be79ebcc157e130b9b3cfcee9
Mohamed ElFayomy <mo.elfayomy@gmail.com>
2017-05-19 16:39:59 +0300

    Initial Commit for the project

---
+++ src/main/java/com/LabDev/LabDevApplication.java
@@ -0,0 +1,12 @@
+package com.LabDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDevApplication.class, args);
+	}
+}
