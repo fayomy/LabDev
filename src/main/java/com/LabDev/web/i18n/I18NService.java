dbe48f17c85c94779cffaff6ac2d3eb5c5d34070
Mohamed ElFayomy <mo.elfayomy@gmail.com>
2017-05-20 02:59:20 +0300

    Complexity

--- src/main/java/com/LabDev/web/i18n/I18NService.java
+++ src/main/java/com/LabDev/web/i18n/I18NService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class I18NService {
 private static final org.slf4j.Logger LOG=org.slf4j.LoggerFactory.getLogger(I18NService.class);
+	
 	@Autowired
 	private MessageSource messageSource;
 
